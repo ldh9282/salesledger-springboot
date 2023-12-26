65726fc0e409a5b9a18d0ab027c9426581285e86
ldh9282 <ldh9282@naver.com>
2023-12-26 23:03:59 +0900

    modify sql log interceptor

--- src/main/java/com/iyf/salesledger/common/lnterceptor/CustomSqlLoggingInterceptor.java
+++ src/main/java/com/iyf/salesledger/common/lnterceptor/CustomSqlLoggingInterceptor.java
@@ -1,9 +1,16 @@
 package com.iyf.salesledger.common.lnterceptor;
 
+import java.lang.reflect.InvocationTargetException;
 import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
+import org.apache.commons.beanutils.PropertyUtils;
 import org.apache.ibatis.executor.statement.StatementHandler;
+import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.mapping.ParameterMapping;
 import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.plugin.Intercepts;
 import org.apache.ibatis.plugin.Invocation;
@@ -17,13 +24,21 @@ public class CustomSqlLoggingInterceptor implements Interceptor {
 
 	@Override
 	public Object intercept(Invocation invocation) throws Throwable {
+		if (log.isInfoEnabled()) {log.info("Start CustomSqlLoggingInterceptor.intercept");}
+		
 		StatementHandler statementHandler = (StatementHandler) invocation.getTarget();
         String originalSql = statementHandler.getBoundSql().getSql();
         Object parameterObject = statementHandler.getBoundSql().getParameterObject();
-        if (log.isInfoEnabled()) {log.info("Start::: CustomSqlLoggingInterceptor.intercept");}
-        if (log.isInfoEnabled()) {log.info("statementHandler ::: getBoundSql ::: getSql \n" + originalSql.replaceAll("\n\t\t", "\n"));}
-        if (log.isInfoEnabled()) {log.info("statementHandler ::: getBoundSql ::: getParameterObject \n" + (parameterObject == null ? "empty parameterObject" : parameterObject));}
+        
+        List<Object> objectList = getObjectList(statementHandler.getBoundSql());
+        Object[] objectArr = changeObjectListToArr(objectList);
+        String paramSql = getParamSql(originalSql, objectArr);
+        
+        if (log.isInfoEnabled()) {log.info("바인딩 전 쿼리 \n" + originalSql.replaceAll("\n\t\t", "\n"));}
+        if (log.isInfoEnabled()) {log.info("바인딩 파라미터 \n" + (parameterObject == null ? "empty parameterObject" : parameterObject));}
+        if (log.isInfoEnabled()) {log.info("바인딩 후 쿼리 \n" + paramSql.replaceAll("\n\t\t", "\n"));}
         if (log.isInfoEnabled()) {log.info("End CustomSqlLoggingInterceptor.intercept");}
+        
         return invocation.proceed();
 	}
 
@@ -38,5 +53,107 @@ public class CustomSqlLoggingInterceptor implements Interceptor {
 	}
 	
 	
+	private static Object[] changeObjectListToArr(List<Object> objectList) {
+		Object[] objectArr = new Object[objectList.size()];
+		
+		for (int i = 0; i < objectList.size(); i++) {
+			objectArr[i] = objectList.get(i);
+		}
+		
+		return objectArr;
+		
+	}
+	
+	private static List<Object> getObjectList(BoundSql bounsql) {
+		List<Object> objectList = new ArrayList<>();
+		
+		Object parameterObject = bounsql.getParameterObject();
+		
+		List<ParameterMapping> parameterMappings = bounsql.getParameterMappings();
+		
+		try {
+			for (ParameterMapping parameterMapping : parameterMappings) {
+				
+				String propertyName = parameterMapping.getProperty();
+				
+				if (PropertyUtils.isReadable(parameterObject, propertyName)) {
+					// VO 객체인 경우 해당 필드 값을 가져와서 리스트에 추가
+					Object object = PropertyUtils.getProperty(parameterObject, propertyName);
+					objectList.add(object);
+				} else { 
+					// VO 객체가 아닌 경우 처리
+					if (parameterObject instanceof Map) { 
+						// Map 형태인 경우 해당 필드 값을 가져와서 리스트에 추가 
+						Object object = ((Map<?, ?>) parameterObject).get(propertyName);
+						objectList.add(object);
+					} else {
+						// 그 외의 경우 파라미터 자체를 리스트에 추가
+						objectList.add(parameterObject);
+					}
+				}
+			}
+		} catch (IllegalAccessException e) {
+			if (log.isInfoEnabled()) {log.info("getObjectList ::: IllegalAccessException ::: " + e);}
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			if (log.isInfoEnabled()) {log.info("getObjectList ::: InvocationTargetException ::: " + e);}
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			if (log.isInfoEnabled()) {log.info("getObjectList ::: NoSuchMethodException ::: " + e);}
+			e.printStackTrace();
+		} catch (Exception e) {
+			if (log.isInfoEnabled()) {log.info("getObjectList ::: Exception ::: " + e);}
+			e.printStackTrace();
+		}
+		
+		
+		return objectList;
+	}
+	
+	private static String getParamSql(String originalSql, Object[] paramArr) {
+		StringBuffer buffer = new StringBuffer();
+		
+		int questionMarkIndex;
+		int questionMarkCnt = 0;
+		int paramIndex = 0;
+		
+		while ((questionMarkIndex = originalSql.indexOf("?")) >= 0) {
+			if (paramIndex == paramArr.length) {
+				break;
+			}
+			
+			// 버퍼에 ? 이전 부분을 추가
+			buffer.append(originalSql.substring(0, questionMarkIndex));
+			
+			// 버퍼에 ? 대체값을 추가
+			if (paramArr[paramIndex] instanceof String) {
+				buffer.append("'" + paramArr[paramIndex]  + "'");
+			} else if (paramArr[paramIndex] instanceof Integer || paramArr[paramIndex] instanceof Long) {
+				buffer.append(paramArr[paramIndex]);
+			} else if (paramArr[paramIndex] == null) {
+				buffer.append("null");
+			} else {
+				buffer.append(paramArr[paramIndex]);
+			}
+			// originalSql 에서 ? 이전 부분을 제거
+			originalSql = originalSql.substring(questionMarkIndex + "?".length());
+			
+			questionMarkCnt++;
+			paramIndex++;
+		}
+		
+		// 버퍼에 ? 이후 남은 originalSql 추가
+		if (!"".equals(originalSql)) {
+			buffer.append(originalSql);
+		} 
+		
+		if (questionMarkCnt == 0) {
+			return originalSql;
+		} else {
+			return buffer.toString();
+		}
+		
+		
+	}
 	
 }
